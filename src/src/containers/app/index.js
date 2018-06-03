ade9069935917b335b335ac4653baff6111ab0da
Debdutto Chakraborty <debduttoc@gmail.com>
2018-06-03 19:43:00 +0530

    Added Nth Sum

--- src/src/containers/app/index.js
+++ src/src/containers/app/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Route, Link } from "react-router-dom";
 import Fibonacci from "../fibonacci";
+import NthSum from "../n-th-sum";
 
 const App = () => (
   <div>
     <header>
       <Link to="/fibonacci">Fibonacci</Link>
+      <span/>
+      <Link to="/n-th-sum">NthSum</Link>
     </header>
 
     <main>
       <Route exact path="/" />
       <Route exact path="/fibonacci" component={Fibonacci} />
+      <Route exact path="/n-th-sum" component={NthSum} />
     </main>
   </div>
 );
---
+++ src/src/containers/n-th-sum/index.js
@@ -0,0 +1,135 @@
+import React from "react"
+import { push } from "react-router-redux"
+import { bindActionCreators } from "redux"
+import { connect } from "react-redux"
+import MIDI from "midi.js"
+import scale from 'music-scale'
+import {
+  increment,
+  incrementAsync,
+  decrement,
+  decrementAsync
+} from "../../modules/counter"
+
+class NthSum extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          <button
+            onClick={this.startMusic}
+          >
+            Start
+          </button>
+
+          <button
+            onClick={this.stopMusic}
+          >
+            Stop
+          </button>
+        </p>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    console.log("MIDIObj", MIDI)
+    MIDI.loadPlugin({
+      soundfontUrl: process.env.PUBLIC_URL + "/soundfont/",
+      instrument: "acoustic_grand_piano",
+      onprogress: function (state, progress) {
+        console.log(state, progress)
+      },
+      onsuccess: function () {
+        prepareNotes("C")
+      }
+    })
+  }
+
+  startMusic() {
+    play = true
+    if (play) {
+      setTimeout(() => {
+        playMusic(1, 1)
+      }, 500)
+    }
+  }
+
+  stopMusic() {
+    play = false
+  }
+}
+
+const firstNum = 1
+const midNum = 1
+const lastNum = 2
+
+const baseBarCount = Number.POSITIVE_INFINITY
+
+let barCount = baseBarCount
+
+const BPM = 80
+const barTime = 60000 / BPM
+const delay = (n) => (n * 0.125 * barTime)
+const notes = []
+const prepareNotes = (selectedScale, startOctave) => {
+  selectedScale = selectedScale ? selectedScale : 'A'
+  startOctave = startOctave ? startOctave : 3
+  let octave = startOctave ? startOctave : 3
+  // let tempNotes = 
+  for (octave; octave < startOctave + 3; octave++) {
+    notes.push(...scale('major', selectedScale + octave))
+  }
+  // for()
+  console.log(notes, notes.length)
+}
+
+let play = true
+
+const playNote = (note, velocity, delay) => {
+  MIDI.noteOn(0, MIDI.keyToNote[note], velocity, delay / 1000)
+  MIDI.noteOff(0, MIDI.keyToNote[note], barTime)
+}
+
+const playMusic = (sum, n) => {
+
+  playNote(notes[sum % 21], (sum % 97) + 30, delay(sum % 8))
+  playNote(notes[(sum % 11) + 10], (sum % 97) + 30, delay(sum % 4))
+
+  let nextSum = generateNthSum(n + 1)
+  if (nextSum < Number.MAX_SAFE_INTEGER && --barCount > 0 && play) {
+    setTimeout(() => {
+      playMusic(nextSum, n + 1)
+    }, barTime)
+  } else if (play) {
+    console.log("seriesReset", nextSum)
+    barCount = baseBarCount
+    setTimeout(() => {
+      playMusic(1, 1)
+    }, barTime)
+  }
+}
+
+const generateNthSum = (n) => {
+  return (n * (n + 1)) / 2
+}
+
+const mapStateToProps = state => ({
+  count: state.counter.count,
+  isIncrementing: state.counter.isIncrementing,
+  isDecrementing: state.counter.isDecrementing
+})
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      increment,
+      incrementAsync,
+      decrement,
+      decrementAsync,
+      changePage: () => push("/about-us")
+    },
+    dispatch
+  )
+
+export default connect(mapStateToProps, mapDispatchToProps)(NthSum)
