4b9b86dc9e5b96190f548d240b25fb5400cfc81e
Debdutto Chakraborty <Debdutto@users.noreply.github.com>
2018-10-07 16:01:06 +0530

    Feature/normalize (#18)
    * Test
    
    * Cleaned up literacy parse code
    
    * Parsed Air Quality (Delhi)
    
    * Cleaned up the script
    
    * Fix script double stringify bug
    
    * Adds jStat
    
    * Added normalize function

---
+++ src/src/modules/normalize.js
@@ -0,0 +1,21 @@
+const jstat = require("jstat").jStat
+
+module.exports = (arr, rangeStart, rangeEnd) => {
+
+    rangeStart = rangeStart ? rangeStart : 1
+    rangeEnd = rangeEnd ? rangeEnd : 100
+
+    let arrMin = jstat.min(arr)
+    let arrMax = jstat.max(arr)
+
+    let rangeDifference = (rangeEnd - rangeStart)
+    let denominator = (arrMax - arrMin)
+
+    // (v - min(v)) * (newupper - newlower) / (max(v) - min(v)) + newlower
+
+    let normalizedArr = arr.map(element => {
+        return Math.round((element - arrMin) * rangeDifference / denominator + rangeStart)
+    })
+
+    return normalizedArr
+}
