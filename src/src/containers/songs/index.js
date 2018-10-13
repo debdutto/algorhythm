ee00aabfbc099e550c514eb7b5573f63e4655adb
Debdutto Chakraborty <debduttoc@gmail.com>
2018-10-13 10:37:39 +0530

    Added links (they don't work yet)

--- src/src/containers/songs/index.js
+++ src/src/containers/songs/index.js
@@ -2,30 +2,68 @@ import React from "react";
 import { connect } from "react-redux";
 import AudioPlayer from "../../elements/AudioPlayer";
 import { SongList } from "../../modules/songs";
-import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+import piPlaces from "../pi/pi-places.json";
 import Grid from "@material-ui/core/Grid";
 
 const songStyles = {
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  height: "100vh"
+  height: "60vh"
 };
+
+const numberStyles = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "32vh",
+  margin: "1vw",
+  overflowX: "hidden"
+};
+
+const StyledButton = withStyles({
+  root: {
+    margin: "10px auto"
+  }
+})(Button);
 
 class Songs extends React.Component {
   render() {
     return (
-      <div style={songStyles}>
-        <Grid container spacing={24} key="songContainer">
-          {SongList.map(el => {
-            let song = [el];
-            return (
-              <Grid item xs={3} key={el.url}>
-                <AudioPlayer songs={song} />
-              </Grid>
-            );
-          })}
-        </Grid>
+      <div>
+        <div style={songStyles}>
+          <Grid container xs={12} spacing={16}>
+            {SongList.map(el => {
+              let song = [el];
+              return (
+                <Grid
+                  container
+                  xs={3}
+                  key={el.url}
+                  alignItems="center"
+                  justify="center"
+                  justifyContent="center"
+                >
+                  <Grid item xs={12}>
+                    <AudioPlayer songs={song} />
+                  </Grid>
+                  <Grid item xs={18}>
+                    <StyledButton variant="contained">
+                      {"Go to " + el.artist.song}
+                    </StyledButton>
+                  </Grid>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </div>
+        <div style={numberStyles}>
+          <Grid item xs={12}>
+            {piPlaces.pi}
+          </Grid>
+        </div>
       </div>
     );
   }
