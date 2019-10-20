e351636dfc7900dc97df513a8a1d99c936caba79
Debdutto Chakraborty <debduttoc@gmail.com>
2019-10-20 21:26:25 +0700

    Fixed Suicide Rates

--- src/src/containers/suicide-rates/index.js
+++ src/src/containers/suicide-rates/index.js
@@ -5,8 +5,10 @@ import {
   playNote,
   startMusic,
   stopMusic,
-  humanize4by4
+  humanize4by4,
+  delay
 } from "../../modules/music";
+import PlayParams from "../../models/playParams";
 import suicideData from "./suicide-data.json";
 import normalize from "../../modules/normalize";
 import { withStyles } from "@material-ui/core/styles";
@@ -17,26 +19,30 @@ class SuicideRates extends React.Component {
   constructor() {
     super();
     this.headlineText = "Suicide Rates";
+
+    this.notes = prepareNotes("B", 2, "major", 5);
+    suicideData.suicides = normalize(suicideData.suicides, 0, 100);
+    this.play = false;
+    this.playParams = new PlayParams(
+      startMusic,
+      this.play,
+      this.notes,
+      playMusic,
+      getSuicideRatesAtN,
+      stopMusic
+    );
   }
 
   render() {
     let { classes } = this.props;
-    return ComplexPlayer(startMusic, stopMusic, classes, this.headlineText);
+    return ComplexPlayer(this.playParams, classes, this.headlineText);
   }
 
   componentDidMount() {
     console.log("MIDIObj", MIDI);
     MIDI.loadPlugin({
       soundfontUrl: process.env.PUBLIC_URL + "/soundfont/",
-      instrument: "acoustic_grand_piano",
-      onprogress: function(state, progress) {
-        console.log(state, progress);
-      },
-      onsuccess: function() {
-        // console.log(piPlaces)
-        suicideData.suicides = normalize(suicideData.suicides, 0, 100);
-        notes = prepareNotes("B", 2, "major", 5);
-      }
+      instrument: "acoustic_grand_piano"
     });
   }
 }
@@ -44,43 +50,44 @@ class SuicideRates extends React.Component {
 const baseBarCount = suicideData.suicides.length;
 let barCount = baseBarCount;
 
-const BPM = 70;
-const barTime = 60000 / BPM;
-const delay = n => n * 0.25 * barTime;
-let notes = [];
-
-let play = false;
-
-const playMusic = (current, next, n) => {
-  humanize4by4(current, next, n, notes, false);
-
-  playNote(notes[current % notes.length], current % 97, delay(current % 8));
+const playMusic = (current, next, n, playParams) => {
+  humanize4by4(current, next, n, playParams);
+  let notes = playParams.notes;
+  let barTime = playParams.barTime;
+  let play = playParams.play;
+  playNote(
+    notes[current % notes.length],
+    current % 97,
+    delay(current % 8, playParams.barTime),
+    playParams
+  );
   playNote(
     notes[(next * current) % notes.length],
     ((next * current) % 97) + 30,
-    delay((next * current) % ((next * current) % 8))
+    delay((next * current) % ((next * current) % 8), playParams.barTime),
+    playParams
   );
   playNote(
     notes[(next + current) % notes.length],
     ((next + current) % 97) + 30,
-    delay((next + current) % ((next + current) % 8))
+    delay((next + current) % ((next + current) % 8), playParams.barTime),
+    playParams
   );
-  // console.log(n)
+
   if (n <= suicideData.suicides.length && --barCount > 0 && play) {
     setTimeout(() => {
-      playMusic(next, getLiteracyAtN(n + 1), n + 1);
+      playMusic(next, getSuicideRatesAtN(n + 1), n + 1, playParams);
     }, barTime);
-    // console.log(barCount, n)
   } else if (play) {
-    console.log("seriesReset", n);
+    console.log("SeriesReset", n);
     barCount = baseBarCount;
     setTimeout(() => {
-      playMusic(getLiteracyAtN(1), getLiteracyAtN(2), 1);
+      playMusic(getSuicideRatesAtN(1), getSuicideRatesAtN(2), 1, playParams);
     }, barTime);
   }
 };
 
-const getLiteracyAtN = n => {
+const getSuicideRatesAtN = n => {
   return suicideData.suicides[n - 1];
 };
 
